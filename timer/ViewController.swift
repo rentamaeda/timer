b8fc762b19f87bf1af5e01059e4aae0c158c646d
maedarenfutoshi <japan_lunch_fate@yahoo.co.jp>
2020-07-08 12:25:42 +0900

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by 前田蓮太 on 2020/07/08.
+//  Copyright © 2020 renta.Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
